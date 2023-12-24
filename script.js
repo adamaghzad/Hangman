b541f8b5a79caef8c1f4aa29317db580bf583c4e
adamaghzad <111640047+adamaghzad@users.noreply.github.com>
2023-12-24 14:51:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,54 +10,89 @@ const resultText = document.getElementById("result-text");
 //Options values for buttons
 let options = {
   fruits: [
-         "Apple",
-        "Blueberry",
-        "Mandarin",
-        "Pineapple",
-        "Pomegranate",
-        "Watermelon",
-        "Banana",
-        "Cherry",
-        "Grapes",
-        "Kiwi",
-        "Orange",
-        "Strawberry",
-        "Mango",
-        "Pear",
-        "Raspberry",
-        "Blackberry",
-        "Peach",
-        "Plum",
-        "Apricot",
+    "تفاح",
+    "توت أزرق",
+    "ماندرين",
+    "أناناس",
+    "رمان",
+    "بطيخ",
+    "موز",
+    "كرز",
+    "عنب",
+    "كيوي",
+    "برتقال",
+    "فراولة",
+    "مانجو",
+    "كمثرى",
+    "توت أحمر",
+    "توت أسود",
+    "خوخ",
+    "برقوق",
+    "مشمش",
   ],
-  animals: ["Hedgehog", "Rhinoceros", "Squirrel", "Panther", "Walrus", "Zebra",
-        "Giraffe", "Kangaroo", "Elephant", "Penguin", "Cheetah", "Ostrich",
-        "Koala", "Gorilla", "Dolphin", "Lion", "Tiger", "Leopard", "Panda",
-        "Polar Bear", "Hippopotamus", "Giraffe", "Koala", "Kangaroo",
-        "Monkey", "Parrot", "Crocodile", "Giraffe", "Penguin", "Peacock",
-        "Sloth", "Toucan", "Pangolin", "Jaguar", "Cobra", "Turtle", "Frog",
-        "Flamingo", "Chameleon", "Pangolin", "Meerkat", "Armadillo", "Cheetah"
-    ],
+  animals: [
+    "قنفذ",
+    "وحيد القرن",
+    "سنجاب",
+    "نمر",
+    "فقمة",
+    "حمار الوحش",
+    "زرافة",
+    "كنغر",
+    "فيل",
+    "بطريق",
+    "فهد",
+    "نعامة",
+    "كوالا",
+    "غوريلا",
+    "دلفين",
+    "أسد",
+    "نمر",
+    "فهد",
+    "باندا",
+    "دب قطبي",
+    "فيمبوبوتاموس",
+    "زرافة",
+    "كوالا",
+    "كنغر",
+    "قرد",
+    "ببغاء",
+    "تمساح",
+    "زرافة",
+    "بطريق",
+    "طاووس",
+    "كسل",
+    "طوقان",
+    "فانغولين",
+    "نمر",
+    "كوبرا",
+    "سلحفاة",
+    "ضفدع",
+    "فلامنجو",
+    "كاميليون",
+    "فانغولين",
+    "ميركات",
+    "أرماديلو",
+    "فهد",
+  ],
   countries: [
-    "India",
-    "Hungary",
-    "Kyrgyzstan",
-    "Switzerland",
-    "Zimbabwe",
-    "Dominica",
+    "الهند",
+    "هنغاريا",
+    "قيرغيزستان",
+    "سويسرا",
+    "زيمبابوي",
+    "دومينيكا",
     "",
-    
   ],
 };
 
-
-//count
+// count
 let winCount = 0;
 let count = 0;
 
 let chosenWord = "";
 
-//Display option buttons
+// Display option buttons
 const displayOptions = () => {
   optionsContainer.innerHTML += `<h3>Please Select An Option</h3>`;
   let buttonCon = document.createElement("div");
@@ -67,26 +102,26 @@ const displayOptions = () => {
   optionsContainer.appendChild(buttonCon);
 };
 
-//Block all the Buttons
+// Block all the Buttons
 const blocker = () => {
   let optionsButtons = document.querySelectorAll(".options");
   let letterButtons = document.querySelectorAll(".letters");
-  //disable all options
+  // disable all options
   optionsButtons.forEach((button) => {
     button.disabled = true;
   });
 
-  //disable all letters
+  // disable all letters
   letterButtons.forEach((button) => {
     button.disabled.true;
   });
   newGameContainer.classList.remove("hide");
 };
 
-//Word Generator
+// Word Generator
 const generateWord = (optionValue) => {
   let optionsButtons = document.querySelectorAll(".options");
-  //If optionValur matches the button innerText then highlight the button
+  // If optionValue matches the button innerText then highlight the button
   optionsButtons.forEach((button) => {
     if (button.innerText.toLowerCase() === optionValue) {
       button.classList.add("active");
@@ -94,93 +129,96 @@ const generateWord = (optionValue) => {
     button.disabled = true;
   });
 
-  //initially hide letters, clear previous word
+  // initially hide letters, clear previous word
   letterContainer.classList.remove("hide");
   userInputSection.innerText = "";
 
   let optionArray = options[optionValue];
-  //choose random word
+  // choose random word
   chosenWord = optionArray[Math.floor(Math.random() * optionArray.length)];
   chosenWord = chosenWord.toUpperCase();
 
-  //replace every letter with span containing dash
-  let displayItem = chosenWord.replace(/./g, '<span class="dashes">_</span>');
+  // replace every letter with span containing dash
+  let displayItem = chosenWord.replace(
+    /./g,
+    '<span class="dashes">_</span>'
+  );
 
-  //Display each element as span
+  // Display each element as span
   userInputSection.innerHTML = displayItem;
 };
 
-//Initial Function (Called when page loads/user presses new game)
+// Initial Function (Called when the page loads/user presses new game)
 const initializer = () => {
   winCount = 0;
   count = 0;
 
-  //Initially erase all content and hide letteres and new game button
+  // Initially erase all content and hide letters and new game button
   userInputSection.innerHTML = "";
   optionsContainer.innerHTML = "";
   letterContainer.classList.add("hide");
   newGameContainer.classList.add("hide");
   letterContainer.innerHTML = "";
 
-  //For creating letter buttons
-  for (let i = 65; i < 91; i++) {
+  // For creating letter buttons
+  for (let i = 0; i < 29; i++) {
     let button = document.createElement("button");
     button.classList.add("letters");
-    //Number to ASCII[A-Z]
-    button.innerText = String.fromCharCode(i);
-    //character button click
+    // Unicode values for Arabic letters
+    button.innerText = String.fromCharCode(1601 + i);
+    // character button click
     button.addEventListener("click", () => {
       let charArray = chosenWord.split("");
       let dashes = document.getElementsByClassName("dashes");
-      //if array contains clciked value replace the matched dash with letter else dram on canvas
+      // if the array contains clicked value replace the matched dash with a letter else draw on the canvas
       if (charArray.includes(button.innerText)) {
         charArray.forEach((char, index) => {
-          //if character in array is same as clicked button
+          // if the character in the array is the same as the clicked button
           if (char === button.innerText) {
-            //replace dash with letter
+            // replace dash with a letter
             dashes[index].innerText = char;
-            //increment counter
+            // increment counter
             winCount += 1;
-            //if winCount equals word lenfth
+            // if winCount equals word length
             if (winCount == charArray.length) {
               resultText.innerHTML = `<h2 class='win-msg'>You Win!!</h2><p>The word was <span>${chosenWord}</span></p>`;
-              //block all buttons
+              // block all buttons
               blocker();
             }
           }
         });
       } else {
-        //lose count
+        // lose count
         count += 1;
-        //for drawing man
+        // for drawing man
         drawMan(count);
-        //Count==6 because head,body,left arm, right arm,left leg,right leg
+        // Count==6 because head, body, left arm, right arm, left leg, right leg
         if (count == 6) {
           resultText.innerHTML = `<h2 class='lose-msg'>You Lose!!</h2><p>The word was <span>${chosenWord}</span></p>`;
           blocker();
         }
       }
-      //disable clicked button
+      // disable clicked button
       button.disabled = true;
     });
     letterContainer.append(button);
   }
 
   displayOptions();
-  //Call to canvasCreator (for clearing previous canvas and creating initial canvas)
+  // Call to canvasCreator (for clearing previous canvas and creating initial canvas)
   let { initialDrawing } = canvasCreator();
-  //initialDrawing would draw the frame
+  // initialDrawing would draw the frame
   initialDrawing();
 };
 
-//Canvas
+// Canvas
 const canvasCreator = () => {
   let context = canvas.getContext("2d");
   context.beginPath();
   context.strokeStyle = "#000";
   context.lineWidth = 2;
 
-  //For drawing lines
+  // For drawing lines
   const drawLine = (fromX, fromY, toX, toY) => {
     context.moveTo(fromX, fromY);
     context.lineTo(toX, toY);
@@ -213,24 +251,24 @@ const canvasCreator = () => {
     drawLine(70, 80, 90, 110);
   };
 
-  //initial frame
+  // initial frame
   const initialDrawing = () => {
-    //clear canvas
+    // clear canvas
     context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-    //bottom line
+    // bottom line
     drawLine(10, 130, 130, 130);
-    //left line
+    // left line
     drawLine(10, 10, 10, 131);
-    //top line
+    // top line
     drawLine(10, 10, 70, 10);
-    //small top line
+    // small top line
     drawLine(70, 10, 70, 20);
   };
 
   return { initialDrawing, head, body, leftArm, rightArm, leftLeg, rightLeg };
 };
 
-//draw the man
+// draw the man
 const drawMan = (count) => {
   let { head, body, leftArm, rightArm, leftLeg, rightLeg } = canvasCreator();
   switch (count) {
@@ -257,6 +295,6 @@ const drawMan = (count) => {
   }
 };
 
-//New Game
+// New Game
 newGameButton.addEventListener("click", initializer);
 window.onload = initializer;
